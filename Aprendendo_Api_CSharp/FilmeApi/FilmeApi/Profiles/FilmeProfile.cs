65344c0ac70cfb4bcd3825fc1af5451dfe0ffc82
Adrielly Vitoria <mendes.silva.vitoria.adrielly@gmail.com>
2023-06-27 13:23:11 -0300

    atualizacoes

---
+++ Aprendendo_Api_CSharp/FilmeApi/FilmeApi/Profiles/FilmeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using FilmeApi.Data.Dtos;
+using FilmeApi.Models;
+
+namespace FilmeApi.Profiles;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile() 
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+    }
+}
